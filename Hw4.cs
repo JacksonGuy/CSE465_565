7f9f9a2f672a2446ec67cd0d704291b0d40cdcaf
JacksonGuy <jacksonf955@gmail.com>
2024-10-29 14:48:18 -0400

    Homework 4 commit 2

--- Hw4.cs
+++ Hw4.cs
@@ -18,6 +18,44 @@ using System.Collections.Generic;
 
 public class Hw4
 {
+    public static void GetCommonCityNames(string[] lines)
+    {
+        // Open states.txt to get states
+        string[] states = File.ReadAllLines("states.txt");
+
+        // List of cities for each state
+        Dictionary<string, List<string>> stateCities = 
+            new Dictionary<string, List<string>>();
+
+        // List of all cities
+        List<string> cities = new List<string>();
+
+        // Iterate over zipcode file lines
+        // and add cities to their respective lists        
+        foreach(string line in lines) {
+            // Get details about the line
+            string[] tokens = line.Split("\t");
+            string city = tokens[3];
+            string state = tokens[4];
+
+            // Don't do anything if line is for state we don't care about
+            if (Array.IndexOf(states, state) == -1) {
+                continue;
+            }
+
+            // Check if dictionary contains key
+            if (!stateCities.ContainsKey(state)) {
+                stateCities.Add(state, new List<string>());
+            }
+
+            // Add to state List and overall List
+            stateCities[state].Add(city);
+            cities.Add(city);
+        }
+
+
+    }
+
     public static void Main(string[] args)
     {
         // Capture the start time
